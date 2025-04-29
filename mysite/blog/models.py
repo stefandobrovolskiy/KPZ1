fafe43e3fc82021f7a364fbba358acf14fca4b0b
oleksandraIl <oleksandra.v.lavruk@ukd.edu.ua>
2025-04-29 21:52:38 +0300

    Add status field to Post model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,10 +2,15 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
     def __str__(self):
         return self.title
